9b74dbba7442db752fb80ca3daa7e6a98d73a2df
tranquyensinh <vn.quyensinh@gmail.com>
2024-05-08 20:14:55 +0700

    save

--- app/Models/Sanpham.php
+++ app/Models/Sanpham.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Sanpham extends Model
+class SanPham extends Model
 {
     use HasFactory;
     protected $table = 'sanpham';
